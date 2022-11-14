c9a48329bb1bf3cc0ebfd5fc3dff3c11c90be9a2
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-11-14 20:35:36 -0300

    Handling errors

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -3,6 +3,7 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/teste', (req: Request, res: Response) => {
+  throw new Error('Error de teste');
   return res.json({ ok: true });
 });
 
