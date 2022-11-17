463691a5b8d57e87058b69f7d83c8cbb5a120b4e
Grazziano Borges Fagundes <grazzianofagundes@gmail.com>
2022-11-17 14:13:17 -0300

    Creating document

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head';
+import styles from '../../styles/Home.module.scss';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <>
+      <Head>
+        <title>Carteira Digital| Login</title>
+        <div>
+          <h1>Carteira Digital</h1>
+        </div>
+      </Head>
+    </>
   );
 }
