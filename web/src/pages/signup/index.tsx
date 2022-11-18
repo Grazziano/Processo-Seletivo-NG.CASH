a80aeb4d6350af9b45540a28fe1618bdf472277c
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-11-18 20:26:19 -0300

    Configuring user registration

--- web/src/pages/signup/index.tsx
+++ web/src/pages/signup/index.tsx
@@ -1,10 +1,27 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import { FormEvent, useState } from 'react';
 import styles from '../../../styles/Home.module.scss';
 import Button from '../../components/ui/Button';
 import { Input } from '../../components/ui/Input';
 
 export default function SignUp() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  async function handleSignUp(event: FormEvent) {
+    event.preventDefault();
+
+    if (!username || !password) {
+      // TODO adicionar tostify
+      alert('Preencha todos os campos');
+      return;
+    }
+
+    setLoading(true);
+  }
+
   return (
     <>
       <Head>
@@ -15,10 +32,20 @@ export default function SignUp() {
         <h1>Crie sua conta</h1>
 
         <div className={styles.login}>
-          <form>
-            <Input type="text" placeholder="Digite o usuário" />
-            <Input type="password" placeholder="Digite a senha" />
-            <Button type="submit" loading={false}>
+          <form onSubmit={handleSignUp}>
+            <Input
+              type="text"
+              placeholder="Digite o usuário"
+              value={username}
+              onChange={({ target }) => setUsername(target.value)}
+            />
+            <Input
+              type="password"
+              placeholder="Digite a senha"
+              value={password}
+              onChange={({ target }) => setPassword(target.value)}
+            />
+            <Button type="submit" loading={loading}>
               Cadastrar
             </Button>
           </form>
