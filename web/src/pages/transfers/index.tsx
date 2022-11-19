058abd5b22ebf1a8593f45d270b19743c635585c
Grazziano Fagundes <grazzianofagundes@gmail.com>
2022-11-19 15:30:31 -0300

    Transferring between users

--- web/src/pages/transfers/index.tsx
+++ web/src/pages/transfers/index.tsx
@@ -1,15 +1,37 @@
 import Head from 'next/head';
 import { FormEvent, useState } from 'react';
+import { toast } from 'react-toastify';
 import Header from '../../components/Header';
+import { setupApiClient } from '../../services/api';
 import styles from './styles.module.scss';
 
 export default function Transaction() {
   const [name, setName] = useState('');
+  const [value, setValue] = useState<number>(0);
 
   async function handleTransfer(event: FormEvent) {
     event.preventDefault();
 
-    alert('TESTE ' + name);
+    if (!name || value <= 0) {
+      toast.warning('É obrigatório informar usuário e valor', {
+        theme: 'colored',
+      });
+      return;
+    }
+
+    const apiClient = setupApiClient();
+
+    await apiClient.post('/cash-out', {
+      username: name,
+      value,
+    });
+
+    toast.success('Tranferência concluída com sucesso!', {
+      theme: 'colored',
+    });
+
+    setName('');
+    setValue(0);
   }
 
   return (
@@ -31,6 +53,14 @@ export default function Transaction() {
               className={styles.input}
               value={name}
               onChange={({ target }) => setName(target.value)}
+            />
+
+            <input
+              type="number"
+              placeholder="Digite o valor a ser transferido"
+              className={styles.input}
+              value={value}
+              onChange={({ target }) => setValue(Number(target.value))}
             />
 
             <button type="submit" className={styles.button}>
